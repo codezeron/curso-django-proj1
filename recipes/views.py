2ad25339c2f782a901cbee6a959ce1d212725606
codezeron <enzoyutakah@gmail.com>
2023-06-11 18:14:37 -0300

    ✨ - feat: aula 46 inserindo dados falsos

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from utils.recipes.factory import make_recipe
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', status=201) 
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)], 
+},status=201) 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Enzo', 
+        'recipe': make_recipe(), 
 }) 
 
 # its calling by namespace's folder called recipes in 'templates'
